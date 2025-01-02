6a16846371079f40689ae33279d9b91eacb70d17
Yan233_ <Yan233th@126.com>
2025-01-03 00:18:46 +0800

    Finish

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from openai import OpenAI
 
 from secret import api_key
 
-
 print("正在启动浏览器")
 options = Options()
 options.add_argument("--disable-logging")
@@ -41,11 +40,11 @@ def check() -> bool:
                 return False
         else:
             return True
-    except WebDriverException:  # 当前页已被关闭
+    except Exception:  # 当前页已被关闭
         print("检测到当前不在问答页, 正尝试在其他页面中查找问答页")
         try:
             all_windows = driver.window_handles
-        except WebDriverException:  # driver已被关闭
+        except Exception:  # driver已被关闭
             print("当前浏览器已被关闭, 程序无法继续执行, 正在退出")
             driver.quit()
             sys.exit(1)
@@ -78,7 +77,7 @@ def ask():
         messages=[
             {
                 "role": "system",
-                "content": "你会收到很多问题作为样例, 你要根据提供的例子输出一定数量同领域相近但不相同的问题, 输出的问题数量将由用户指定, 单个问题在一行内不要换行, 输出的问题以换行分隔, 不要使用Markdown或LaTeX语法, 切记要遵循问题数量",
+                "content": "你会收到很多问题作为样例, 你要根据提供的例子输出一定数量同领域相近但不相同的问题, 输出的问题数量将由用户指定, 单个问题需要在一行内不换行输出, 输出的问题以换行分隔, 不要使用Markdown或LaTeX语法, 切记要遵循问题数量",
             },
             {"role": "user", "content": ai_question},
         ],
@@ -96,8 +95,9 @@ def ask():
     for question in questions_list:
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "ask-btn"))).click()  # 打开输入框
         WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.TAG_NAME, "textarea"))).send_keys(question)
-        time.sleep(2)
+        time.sleep(2.5)
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".up-btn.ZHIHUISHU_QZMD.set-btn"))).click()
+        time.sleep(1)
 
 
 @reloading
@@ -107,12 +107,19 @@ def answer():
     course_name = driver.find_element(By.CLASS_NAME, "course-name").text
     print(f"课程名: {course_name}")
     print("鉴于智慧树的屏蔽机制, 建议回答总数量在25个以上 (20个有效回答为有效计分上限)")
-    replies = int(input("请输入本次回答数量: "))
-    print(f"正在获取页面中前{replies}个问题并发给AI分析")
-    question_elements = driver.find_elements(By.CLASS_NAME, "question-content")[:replies]
+    print("请输入本次回答问题序号区间 (索引从0起)")
+    start = int(input("From: "))
+    end = int(input("To: "))
+    if start > end:
+        print("请输入正确的区间!")
+        return
+    replies = end - start + 1
+    print(f"正在获取页面中第{start}~{end}个问题并发给AI分析")
+    question_elements = driver.find_elements(By.CLASS_NAME, "question-content")[start : end + 1]
     question_text = ""
     question_title = []
     for question in question_elements:
+        print(question.text)
         question_text += question.text + "\n"  # 将所有元素的文本合并
         question_title.append(question.text)
     ai_question = "请根据提供的问题生成对应的回答, 以下为问题:\n" + question_text
@@ -121,7 +128,7 @@ def answer():
         messages=[
             {
                 "role": "system",
-                "content": "你会收到一些问题, 你要根据问题输出对应的回答, 单个问题的回答在一行内不要换行, 输出的问题以换行分隔, 不要使用Markdown或LaTeX语法, 切记要按照问题数量一对一地输出回答",
+                "content": "你会收到一些问题, 你要根据问题输出对应的回答, 单个问题的回答需要在一行内不换行输出, 输出的问题以换行分隔, 不要使用Markdown或LaTeX语法",
             },
             {"role": "user", "content": ai_question},
         ],
@@ -137,7 +144,7 @@ def answer():
         case _:
             return
     ori_page = driver.current_window_handle
-    for question in question_title:
+    for answer, question in zip(answers_list, question_title):
         window_handles_before = driver.window_handles
         driver.find_element(By.CSS_SELECTOR, f'div[title="{question}"]').click()
         time.sleep(1)
@@ -148,12 +155,15 @@ def answer():
         driver.switch_to.window(new_page)
         try:
             WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "my-answer-btn"))).click()  # 打开输入框
-        except WebDriverException:
-            print ("本题无法回答, 可能是已经回答过")
+        except Exception:
+            print(f"本题无法回答, 可能是已经回答过, 题目: {question}")
+            driver.close()
+            driver.switch_to.window(ori_page)
             continue
-        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.TAG_NAME, "textarea"))).send_keys(question)
-        time.sleep(2)
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.TAG_NAME, "textarea"))).send_keys(answer)
+        time.sleep(2.5)
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".up-btn.ZHIHUISHU_QZMD.set-btn"))).click()
+        print(f"成功回答问题: {question}")
         time.sleep(1)
         driver.close()
         driver.switch_to.window(ori_page)
