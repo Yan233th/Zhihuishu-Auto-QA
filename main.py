1a7af2d0ef2e9d8dc673c78e45b02282d266d9d1
Yan233_ <Yan233th@126.com>
2025-01-03 00:23:29 +0800

    Yeeeeeeeeeeeeees

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from selenium.common.exceptions import WebDriverException
 from openai import OpenAI
 
 from secret import api_key
